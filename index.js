c0105a869f645bfa6d866ab483dd209848ce9b28
Paul Nakata <paul@pmn.org>
2016-05-17 13:56:37 -0700

    Have a user send in their access token so that the checker can post a status back

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var sentiment = require('sentiment');
-var Octokat = require('octokat');
-var octo = new Octokat({token: process.env.GITHUB_ACCESS_TOKEN});
+var request = require('request');
 
 function positive_sentiment(author, comment){
   if (indexOf(author, comment) >= 0){
@@ -15,11 +14,27 @@ function positive_sentiment(author, comment){
   }
 }
 
-function report_status(repo, sha, status, message) {
+function report_status(access_token, owner, repo, sha, status, message) {
   // TODO report passing or failing status back to the PR
+  response = {
+    "state": status,
+    "description": message,
+    "context": "manners/golden-rule"
+  }
+
+  response_url = `https://api.github.com/v3/repos/${repo}/statuses/${sha}`;
+
+  request.post(response_url, response);
 }
 
 exports.commentHandler = function(event, context, callback) {
+  access_token = event.token;
+
+  if (access_token == null){
+    callback(null, "failure: Token is missing");
+    return;
+  }
+
   comment = JSON.parse(event);
   pull_request_creator = comment.pull_request.user.login;
   comment_body = comment.body;
@@ -27,12 +42,13 @@ exports.commentHandler = function(event, context, callback) {
   pull_request_id = comment.pull_request.id;
   repo = comment.repository.full_name;
   sha = comment.pull_request.head.sha;
+
   if (positive_sentiment(pull_request_creator, comment_body)) {
     status_message = `Nobody has complimented ${pull_request_creator} yet.`;
-    report_status(repo, sha, "failure", status_message);
+    report_status(token, repo, sha, "failure", status_message);
   } else {
     status_message = `Hooray! ${commenter} said something nice about ${pull_request_creator}!`;
-    report_status(repo, sha, "success", status_message);
+    report_status(token, repo, sha, "success", status_message);
   }
 
   callback(null, "success");
